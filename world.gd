fbca8477d6c03c569bf91e5bb0a9e86d29aebcd0
fizzydrinks <gabriel@lins.dev>
2023-09-30 17:28:37 -0300

    Add village/dungeon generation

---
+++ world.gd
@@ -0,0 +1,105 @@
+extends Node2D
+
+
+const TILE_SIZE = 64
+const STRUCTURE_MAX_DENSITY = 4
+const STRUCTURE_MAX_GEN_CHANCE = 0.5
+const STRUCTURE_TYPES_WEIGHTED = {
+	0.75: preload("res://dungeon.tscn"),
+	1: preload("res://village.tscn")
+}
+
+
+var structure_noise = FastNoiseLite.new()
+var terrain_noise = FastNoiseLite.new()
+var tiles = {}
+var structures = { 0: { 0: true } }
+var time: float = 0
+
+
+@onready var tile_scene = load("res://tile.tscn")
+@onready var terrain = $terrain
+
+
+func _ready():
+	terrain_noise.noise_type = 3
+	terrain_noise.frequency = 0.07
+	terrain_noise.seed = Time.get_unix_time_from_system()
+	
+	structure_noise.noise_type = 5
+	structure_noise.frequency = 0.1
+	structure_noise.seed = Time.get_unix_time_from_system()
+
+	generate_tiles(Vector2(-10, -10), Vector2(10, 10))
+
+
+func generate_tiles(start: Vector2, end: Vector2):
+	for x in range(start.x, end.x):
+		for y in range(start.y, end.y):
+			generate_or_update_tile(x, y)
+			generate_structures(x, y)
+
+
+func get_terrain_value(x: int, y: int):
+	var tile_value = terrain_noise.get_noise_2d(x, y)
+	return (tile_value + 1) / 2
+
+
+func get_structure_value(x: int, y: int):
+	var tile_value = structure_noise.get_noise_2d(x, y)
+	return (tile_value + 1) / 2
+
+
+func generate_or_update_tile(x: int, y: int):
+	var terrain_value = get_terrain_value(x, y)
+	var green = 0.2 + terrain_value * 0.6
+	var red = terrain_value * green
+	var color = Color(red, green, 0)
+
+	if x not in tiles:
+		tiles[x] = {}
+
+	if y in tiles[x]:
+		tiles[x][y].modulate = color
+		return
+	
+	var tile: Sprite2D = tile_scene.instantiate()
+	tile.modulate = color
+	terrain.add_child(tile)
+	tile.global_position = Vector2(x, y) * TILE_SIZE
+	tile.scale = Vector2.ONE * TILE_SIZE
+	
+	tiles[x][y] = tile
+	
+	
+func generate_structures(x: int, y: int):
+	var structure_value = get_structure_value(x, y)
+	
+	if randf() >= structure_value * STRUCTURE_MAX_GEN_CHANCE:
+		return
+		
+	for checkx in range(x - STRUCTURE_MAX_DENSITY, x + STRUCTURE_MAX_DENSITY):
+		if not checkx in structures:
+			continue
+
+		for checky in range(y - STRUCTURE_MAX_DENSITY, y + STRUCTURE_MAX_DENSITY):
+			if not checky in structures[checkx]:
+				continue
+				
+			return
+
+	var structure_type = randf()
+	var structure: Node2D
+	for chance in STRUCTURE_TYPES_WEIGHTED.keys():
+		if structure_type < chance:
+			structure = STRUCTURE_TYPES_WEIGHTED[chance].instantiate()
+			break
+
+	add_child(structure)
+	structure.global_position = Vector2(x, y) * TILE_SIZE
+	
+	if x not in structures:
+		structures[x] = {}
+		
+	if y not in structures[x]:
+		structures[x][y] = structure
